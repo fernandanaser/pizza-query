183bfae03d3540670a5c24b5395b144ac4fd16e8
fernandamendesnaser@gmail.com <fernandamendesnaser@gmail.com>
2023-05-25 00:10:59 +0000

    Olá mundo do primeiro dia
    Foi feito um seletc de Olá mundo e uma busca na tabela de pedido

---
+++ olaMundo.sql
@@ -0,0 +1,8 @@
+-- Databricks notebook source
+SELECT 'Olá mundo!'
+
+-- COMMAND ----------
+
+SELECT * 
+FROM silver.pizza_query.pedido
+--seleciona todas as colunas da tabela (catalago.database.tabela)
